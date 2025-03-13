6204183c35e65fb60d93438c5cde097d1a36b14f
shubham pande <145204677+shubhamspande@users.noreply.github.com>
2025-03-13 13:32:22 +0530

    Update provider.tf

--- modules/infrastructure/provider.tf
+++ modules/infrastructure/provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region = var.aws_region
+  region                  = "us-east-1"  # Change to your preferred region
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                 = "default"  # Uses the profile set up by `aws configure`
 }
